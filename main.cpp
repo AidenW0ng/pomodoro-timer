0f4189aa692ea92398533bc4ae7d2de691c682c5
Aiden Wong <aidenpwong@gmail.com>
2023-09-23 15:26:41 -0700

    basic timer in corner

--- main.cpp
+++ main.cpp
@@ -2,30 +2,28 @@
 
 int main() {
     sf::RenderWindow window(sf::VideoMode(800, 600), "SFML Event Example");
-    sf::Event event;
-
+    sf::Clock clock;
+    sf::Time time = clock.getElapsedTime();
     // Set the initial background color to white
     sf::Color bgColor = sf::Color::White;
-
+    sf::Font font;
+    font.loadFromFile("Arial.ttf");
+    sf::Text text(std::to_string(time.asSeconds()), font);
+    text.setCharacterSize(30);
+    text.setFillColor(sf::Color::Black);
     while (window.isOpen()) {
+        sf::Event event;
         while (window.pollEvent(event)) {
             if (event.type == sf::Event::Closed) {
-		window.close();
-            } else if (event.type == sf::Event::KeyPressed) {
-                // Change background color when a key is pressed
-                if (event.key.code == sf::Keyboard::R) {
-                    bgColor = sf::Color::Red;
-                } else if (event.key.code == sf::Keyboard::G) {
-                    bgColor = sf::Color::Green;
-                } else if (event.key.code == sf::Keyboard::B) {
-                    bgColor = sf::Color::Blue;
-                }
-            }
+                window.close();
+            } 
+            
         }
-
+        time = clock.getElapsedTime();
+        text.setString(std::to_string(time.asSeconds()));
         // Clear the window with the current background color
         window.clear(bgColor);
-
+        window.draw(text);
         // Display the contents of the window
         window.display();
     }
