31e4a2922c5657cdc05bdb306ce31ecc15cfcd16
Aiden Wong <aidenpwong@gmail.com>
2023-09-23 17:04:40 -0700

    put text somewhere else, press escape to leave

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,14 @@
 #include <SFML/Graphics.hpp>
+#include <iostream>
 
-int main() {
+float two_des(float var)
+{
+    float value = (int) (var * 100 + 0.5);
+    return (float) value / 100;
+}
+
+int main()
+{
     sf::RenderWindow window(sf::VideoMode(800, 600), "SFML Event Example");
     sf::Clock clock;
     sf::Time time = clock.getElapsedTime();
@@ -8,7 +16,8 @@ int main() {
     sf::Color bgColor = sf::Color::White;
     sf::Font font;
     font.loadFromFile("Arial.ttf");
-    sf::Text text(std::to_string(time.asSeconds()), font);
+    sf::Text text(std::to_string((int) two_des(time.asSeconds())), font);
+    text.setPosition(500, 300);
     text.setCharacterSize(30);
     text.setFillColor(sf::Color::Black);
     while (window.isOpen()) {
@@ -16,11 +25,16 @@ int main() {
         while (window.pollEvent(event)) {
             if (event.type == sf::Event::Closed) {
                 window.close();
-            } 
-            
+            }
+            else if(event.type == sf::Event::KeyPressed)
+            {
+                if(sf::Keyboard::isKeyPressed(sf::Keyboard::Escape))
+                   window.close();
+            }
+
         }
         time = clock.getElapsedTime();
-        text.setString(std::to_string(time.asSeconds()));
+        text.setString(std::to_string((int) two_des(time.asSeconds())));
         // Clear the window with the current background color
         window.clear(bgColor);
         window.draw(text);
