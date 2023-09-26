83939d988e2be21cb8b2e5c3f421a0b5a3ea6d1a
Aiden Wong <aidenpwong@gmail.com>
2023-09-26 13:10:54 -0700

    start/stop button and countdown from 60

--- src/main.cpp
+++ src/main.cpp
@@ -1,31 +1,43 @@
 #include <SFML/Graphics.hpp>
 #include <iostream>
-
-float two_des(float var)
-{
-    float value = (int) (var * 100 + 0.5);
-    return (float) value / 100;
-}
+#include <vector>
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(800, 600), "Timer");
+    bool clock_running = false;
+
+
+    sf::RenderWindow window(sf::VideoMode(800, 800), "Timer");
     sf::Clock clock;
-    sf::Time time = clock.getElapsedTime();
+    sf::Time time = sf::seconds(60.0);
     // Set the initial background color to white
     sf::Color bgColor = sf::Color::White;
     sf::Font font;
     font.loadFromFile("Arial.ttf");
-    sf::Text text(std::to_string((int) two_des(time.asSeconds())), font);
-    text.setPosition(500, 300);
+    sf::Text text(std::to_string((int) (time.asSeconds())), font);
+    text.setPosition(300, 300);
+    text.setFillColor(sf::Color::Black);
+    text.setCharacterSize(15);
 
     sf::Text text_reset("Reset", font);
     text_reset.setCharacterSize(15);
     text_reset.setFillColor(sf::Color::Black);
     text_reset.setPosition(205,200);
+
+    sf::Text text_stop("Start", font);
+    text_stop.setCharacterSize(15);
+    text_stop.setFillColor(sf::Color::Black);
+    text_stop.setPosition(505, 500);
+
     sf::Vector2f button_pos = text.getPosition();
-    text.setCharacterSize(30);
-    text.setFillColor(sf::Color::Black);
+
+
+    sf::RectangleShape stop_button;
+    stop_button.setSize(sf::Vector2f(50,20));
+    stop_button.setOutlineColor(sf::Color::Black);
+    stop_button.setOutlineThickness(1);
+    stop_button.setPosition(500,500);
+
     sf::RectangleShape button;
     button.setSize(sf::Vector2f(50,20));
     button.setOutlineColor(sf::Color::Black);
@@ -34,6 +46,9 @@ int main()
     button.setFillColor(sf::Color::Transparent);
     while (window.isOpen()) {
         sf::Event event;
+        sf::Vector2i mouse_pos = sf::Mouse::getPosition(window);
+        sf::FloatRect buttonBounds = button.getGlobalBounds();
+        sf::FloatRect stopBounds = stop_button.getGlobalBounds();
         while (window.pollEvent(event)) {
             if (event.type == sf::Event::Closed) {
                 window.close();
@@ -46,24 +61,38 @@ int main()
 
             else if(event.type == sf::Event::MouseButtonPressed)
             {
-                sf::Vector2i mouse_pos = sf::Mouse::getPosition(window);
-                sf::FloatRect buttonBounds = button.getGlobalBounds();
                 if(buttonBounds.contains(static_cast<sf::Vector2f>(mouse_pos)))
-                    clock.restart();
+                    time = sf::seconds(0.0f);
+                if(stopBounds.contains(static_cast<sf::Vector2f>(mouse_pos)))
+                {
+                    clock_running = !clock_running;
+                    if(clock_running)
+                        text_stop.setString("Stop");
+                    else
+                        text_stop.setString("Start");
+                }
             }
-
         }
-        time = clock.getElapsedTime();
-        text.setString(std::to_string((int) two_des(time.asSeconds())));
-        // Clear the window with the current background color
+        if(clock_running)
+        {
+            time -= clock.getElapsedTime();
+            clock.restart();
+        } else
+            clock.restart();
+        text.setString(std::to_string((int) (time.asSeconds())));
+        if(buttonBounds.contains(static_cast<sf::Vector2f>(mouse_pos)))
+            button.setFillColor(sf::Color::Green);
+        else
+           button.setFillColor(sf::Color::Transparent);
+
         window.clear(bgColor);
-        window.draw(text);
-        window.draw(text_reset);
         window.draw(button);
-        // Display the contents of the window
+        window.draw(text_reset);
+        window.draw(stop_button);
+        window.draw(text_stop);
+        window.draw(text);
         window.display();
     }
 
     return 0;
-}
-
+    }
